6624906643ae037edbef304ed44a1da69e6cbc94
Luigi160397 <luigi.difraia.97@gmail.com>
2023-06-19 11:36:04 +0200

    base progetto creata

---
+++ src/test/java/epicode/bw5/Bw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.bw5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
