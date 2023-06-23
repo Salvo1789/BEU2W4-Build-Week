76057c865c3a83d7cbc2ba2765f2bcf02de78859
“Salvo1789” <salvo.mercurio89@gmail.com>
2023-06-23 11:56:49 +0200

    Aggiunti endpoint Patch per indirizzo e fattura

--- src/main/java/epicode/bw5/controllers/IndirizziController.java
+++ src/main/java/epicode/bw5/controllers/IndirizziController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -48,7 +49,7 @@ public class IndirizziController {
 		return indirizziService.findByIdAndUpdate(id, body);
 	}
 
-	@PutMapping("/{id}/assegna-cliente")
+	@PatchMapping("/{id}")
 	@PreAuthorize("hasAuthority('ADMIN')")
 	public Indirizzo AssegnaCliente(@PathVariable UUID id, @RequestBody AssegnaIndirizzoPayload body) {
 		return indirizziService.assegnaCliente(id, body);
